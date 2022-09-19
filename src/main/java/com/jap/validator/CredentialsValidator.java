df056ac6c7fec976fea3a1d9e8901b56c2b51439
M V K <krishna.mugala9@gmail.com>
2022-09-19 21:02:37 +0530

    changes made

--- src/main/java/com/jap/validator/CredentialsValidator.java
+++ src/main/java/com/jap/validator/CredentialsValidator.java
@@ -1,5 +1,6 @@
 package com.jap.validator;
 
+// Defining an interface.
 public interface CredentialsValidator {
    public String validator(String userid, String password);
 }
--- src/main/java/com/jap/validator/CredentialsValidatorImpl.java
+++ src/main/java/com/jap/validator/CredentialsValidatorImpl.java
@@ -1,9 +1,16 @@
 package com.jap.validator;
 
 public class CredentialsValidatorImpl {
+    // A method that takes two parameters and returns a string.
     public String validateUser(String name, String password) {
-        return "";
+        CredentialsValidator validator = (userId, password1) -> {
+            if (userId.equalsIgnoreCase("admin") && (password1.equalsIgnoreCase("1234"))) {
+                return "valid user";
+            } else {
+                return "invalid user";
+            }
+        };
+        return validator.validator(name, password);
     }
-
-
 }
+
